4605923dcff6b8faa184877e9c8cef2a4facee44
Jarosław Leśniak <jarek9244@gmail.com>
2019-07-31 16:13:58 +0200

    Change footer style

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -4,7 +4,7 @@ class Footer extends React.Component {
     render() {
         return (
             <footer>
-                <div className="">Created by <a href="../">Jarosław Leśniak</a></div>
+                <div className=""><i>Created by <a href="../">Jarosław Leśniak</a></i></div>
             </footer>
         );
     }
