d9a2df884951d806c72c9608b85994c739db8531
Jarosław Leśniak <jarek9244@gmail.com>
2019-07-31 13:34:02 +0200

    Added react app

--- backend/notes.php
+++ backend/notes.php
@@ -1,5 +1,24 @@
 <?php
 
+    header("Content-type: application/json; charset=utf-8");
     include "./connection.php";
 
-    echo Connection::HOST;
+    $pdo = new PDO('mysql:host='.Connection::HOST.';dbname='.Connection::DATABASE.';charset=utf8', Connection::USER, Connection::PASSWORD);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  
+
+    $conenctionType = $_SERVER['REQUEST_METHOD'];
+
+    switch($conenctionType) {
+        case "GET":
+            
+        break;
+        case "POST":
+
+        break;
+        case "PUT":
+
+        break;
+        case "DELETE":
+
+        break;
+    }
